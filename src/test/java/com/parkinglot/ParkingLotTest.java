64356d1652d5c2609370187710c86c9c32bbe4cf
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-06 14:33:54 +0800

    [ParkingLot][Mark] test: Added unit test - should_return_car_when_fetch_given_parked_car_and_parking_lot()

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -2,9 +2,11 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingLotTest {
+
     @Test
     void should_return_parkingTicket_when_park_given_parking_lot_and_car(){
         //given
@@ -16,6 +18,20 @@ public class ParkingLotTest {
 
         //then
         assertNotNull(parkingTicket);
+    }
+
+    @Test
+    void should_return_car_when_fetch_given_parked_car_and_parking_lot(){
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        ParkingTicket parkingTicket = parkingLot.park(car);
+
+        //when
+        Car actualCar = parkingLot.fetch(parkingTicket);
+
+        //then
+        assertEquals(car, actualCar);
     }
 
 
