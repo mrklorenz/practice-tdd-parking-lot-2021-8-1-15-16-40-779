3e31eb2c8c1c11d88cb47c6da69a734d9b086712
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-06 14:39:21 +0800

    [ParkingLot][Mark] test: Added unit test - should_return_right_car_when_fetch_twice_given_two_parked_car_and_two_parking_tickets_and_parking_lot()

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -34,5 +34,25 @@ public class ParkingLotTest {
         assertEquals(car, actualCar);
     }
 
+    @Test
+    void should_return_right_car_when_fetch_twice_given_two_parked_car_and_two_parking_tickets_and_parking_lot(){
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car firstCar = new Car();
+        Car secondCar = new Car();
+        ParkingTicket firstCarTicket = parkingLot.park(firstCar);
+        ParkingTicket secondCarTicket = parkingLot.park(secondCar);
+
+        //when
+        Car actualFirstCar = parkingLot.fetch(firstCarTicket);
+        Car actualSecondCar = parkingLot.fetch(secondCarTicket);
+
+        //then
+        assertEquals(firstCar, actualFirstCar);
+        assertEquals(secondCar, actualSecondCar);
+    }
+
+
+
 
 }
