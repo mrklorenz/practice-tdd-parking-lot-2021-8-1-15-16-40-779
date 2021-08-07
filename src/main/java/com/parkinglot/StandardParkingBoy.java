e4f9173c4e1e0cabb3a93883545de22eaa9ec178
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-07 19:55:32 +0800

    [ParkingLot][Mark] feat: Added StandardParkingBoy.java and created has-a relationship with Parkinglot

---
+++ src/main/java/com/parkinglot/StandardParkingBoy.java
@@ -0,0 +1,16 @@
+package com.parkinglot;
+
+public class StandardParkingBoy {
+    private ParkingLot parkingLot;
+
+    public StandardParkingBoy (ParkingLot parkingLot){
+        this.parkingLot = parkingLot;
+    }
+
+    public ParkingTicket park(Car car){
+        return parkingLot.park(car);
+    }
+
+
+
+}
