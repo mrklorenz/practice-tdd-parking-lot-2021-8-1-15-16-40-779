ffeee1b14e2b03ad8f3ec6e5e48374a053f9e020
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-09 23:52:00 +0800

    [ParkingLot][Mark] test: Added unit test - should_return_car_when_fetch_given_smart_parking_boy_parking_lot_and_parking_ticket()

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SmartParkingBoyTest {
+
     @Test
     void should_return_parkingTicket_when_park_given_2_parking_lot_and_car_smart_parkingboy(){
         //given
@@ -27,4 +28,29 @@ class SmartParkingBoyTest {
         //then
         assertNotNull(parkingTicket);
     }
+
+    @Test
+    void should_return_car_when_fetch_given_smart_parking_boy_parking_lot_and_parking_ticket() {
+        //given
+        ParkingLot firstParkingLot  = new ParkingLot(1);
+        ParkingLot secondParkingLot = new ParkingLot(1);
+        Car car = new Car();
+        List<ParkingLot> parkingLots = new ArrayList<>();
+
+        parkingLots.add(firstParkingLot);
+        parkingLots.add(secondParkingLot);
+
+        SmartParkingBoy parkingBoy = new SmartParkingBoy(parkingLots);
+        ParkingTicket parkingTicket = parkingBoy.park(car);
+
+        //when
+        Car actualCar = parkingBoy.fetch(parkingTicket);
+
+        //then
+        assertEquals(car, actualCar);
+    }
+
+
+
+
 }
